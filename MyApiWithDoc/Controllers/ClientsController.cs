7d69b5bb0219eabd9d900960889467c1cb875bdd
Angelito Casagrande <angelito@angelito.com.br>
2020-05-09 15:13:57 -0300

    Fixes

--- MyApiWithDoc/Controllers/ClientsController.cs
+++ MyApiWithDoc/Controllers/ClientsController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mime;
@@ -108,32 +107,27 @@ namespace MyApiWithDoc.Controllers
         [Produces(MediaTypeNames.Application.Json)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public ActionResult<ClientResponse> GetAll()
+        public ActionResult<IEnumerable<ClientResponse>> GetAll()
         {
-            try
-            {
-                var clientsResponse = NoFilter();
-                if (clientsResponse == null)
-                    return NoContent();
+            var clientsResponse = NoFilter();
+            if (clientsResponse == null)
+                return NoContent();
 
-                return Ok(clientsResponse);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            return Ok(clientsResponse);
         }
 
         /// <summary>
         /// Add a new client
         /// </summary>
-        /// <param name="clientRequest"></param>
+        /// <param name="clientRequest">Client data</param>
         /// <returns></returns>
         [HttpPost]
         [Consumes(MediaTypeNames.Application.Json)]
+        [Produces(MediaTypeNames.Application.Json)]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public ActionResult<int> Post([FromBody] CreateClientRequest clientRequest)
+        public ActionResult<int> Post(
+            [FromBody] CreateClientRequest clientRequest)
         {
             if (!ModelState.IsValid)
                 return BadRequest();
@@ -152,13 +146,14 @@ namespace MyApiWithDoc.Controllers
         }
 
         /// <summary>
-        /// Update an existing client
+        /// Update an existing clients
         /// </summary>
         /// <param name="id">Client id</param>
-        /// <param name="clientRequest"></param>
+        /// <param name="clientRequest">Client data</param>
         /// <returns></returns>
         [HttpPut("{id}")]
         [Consumes(MediaTypeNames.Application.Json)]
+        [Produces(MediaTypeNames.Application.Json)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -184,12 +179,13 @@ namespace MyApiWithDoc.Controllers
         }
 
         /// <summary>
-        /// Enable or disable a client
+        /// Enable or disable a client by client id
         /// </summary>
         /// <param name="id">Client id</param>
-        /// <param name="enabled">Enable/Disable</param>
+        /// <param name="enabled">true or false</param>
         /// <returns></returns>
         [HttpPatch("{id}")]
+        [Produces(MediaTypeNames.Application.Json)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -214,13 +210,14 @@ namespace MyApiWithDoc.Controllers
         /// <summary>
         /// Remove a client by client id
         /// </summary>
-        /// <param name="id"></param>
+        /// <param name="id">Client id</param>
         /// <returns></returns>
         [HttpDelete("{id}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<int> Delete([FromRoute] int id)
+        public IActionResult Delete(
+            [FromRoute] int id)
         {
             if (!ModelState.IsValid)
                 return BadRequest();
